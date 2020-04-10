0d6135fddbe653e2f6d8f5dc48595c94e031b9d2
Kezir <Beniamin3@op.pl>
2020-04-10 13:02:37 +0200

    Nie skonczone

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Benia\PycharmProjects\Blog_zdjecia_projekt\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
